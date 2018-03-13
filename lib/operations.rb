55061e376762984780686142a19ae5e11c433ada
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-13 02:04:45 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,10 @@
 def unsafe?(speed)
 	if speed < 40 || speed > 60
 		return true
-	else 
+	else
 		return false
 end
 
 def not_safe?(speed)
-	(speed < 40 || speed > 60) ? true : false
+	return (speed < 40 || speed > 60) ? true : false
 end
-	
-
-
